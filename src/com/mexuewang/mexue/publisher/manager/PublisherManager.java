2e3eaf90b2c556bcfd08979d0ee0f3082c6a9543
sueprluohao <271810625@qq.com>
2016-08-04 19:45:38 +0800

    Add files via upload

--- src/com/mexuewang/mexue/publisher/manager/PublisherManager.java
+++ src/com/mexuewang/mexue/publisher/manager/PublisherManager.java
@@ -5,20 +5,25 @@ import java.util.List;
 import android.content.Context;
 import android.util.SparseArray;
 
+import com.mexuewang.mexue.publisher.PublisherConstants;
 import com.mexuewang.mexue.publisher.config.PublishElementTypeConfig;
 import com.mexuewang.mexue.publisher.config.PublisherElementConfig;
 import com.mexuewang.mexue.publisher.config.PublisherElementRequiredConfig;
 import com.mexuewang.mexue.publisher.element.PublisherElement;
+import com.mexuewang.mexue.publisher.entity.Accessory;
+import com.mexuewang.mexue.publisher.entity.Draft;
+import com.mexuewang.mexue.publisher.entity.EditTextAccessory;
 import com.mexuewang.mexue.publisher.listener.PublisherManagerListener;
 /**
  * @author luohao created on 08/03/2016
- * functioin
+ * functioin:manage draft and elements
  *
  */
 public abstract class PublisherManager {
 	protected SparseArray<PublisherElement> mElements = new SparseArray<PublisherElement>();
 	protected PublisherManagerListener mPublisherManagerListener;
 	protected Context mContext;
+	protected Draft mDraft;
 	public void init(int publishType,Context context) {
 		this.mContext=context;
 		buildElement(publishType);
@@ -43,4 +48,22 @@ public abstract class PublisherManager {
 	public PublisherElement getELement(int type){
 		return mElements.get(type);
 	}
+	/**f
+	 * @param type
+	 * @return get specific accessory by publisher type  eg:editTextAccessory
+	 */
+	public <T extends Accessory> T getAccessory(int type){
+		PublisherElement publisherElement=mElements.get(type);
+		if (null==publisherElement){
+			return null;
+		}
+		return publisherElement.getAccessary();
+	}
+	/**
+	 * function:the entrance of publisher sending growth,notification,homework.
+	 */
+	public void send(){
+		EditTextAccessory editTextAccessory=getAccessory(PublisherConstants.ELEMENT_EDITTEXT);
+		editTextAccessory.getContent();
+	};
 }
